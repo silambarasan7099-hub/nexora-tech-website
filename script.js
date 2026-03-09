67c94acb20ab272cf8dc666440263d0bd817e596
silambarasan7099-hub <silambarasan7099@gmail.com>
2026-03-09 18:03:58 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+particlesJS("particles-js", {
+"particles": {
+"number": { "value": 80 },
+"color": { "value": "#a855f7" },
+"shape": { "type": "circle" },
+"opacity": { "value": 0.5 },
+"size": { "value": 3 },
+"move": { "enable": true, "speed": 2 }
+}
+});
